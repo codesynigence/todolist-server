29786cf8622b2503b50488f357bff7d8fa360cd5
appusf <42931730+appusf@users.noreply.github.com>
2018-09-28 14:37:01 +0530

    Added Contents

---
+++ js/todo.js
@@ -0,0 +1,171 @@
+var st;
+getData();
+
+// Search in databse to get reuired string
+function searchfunc() {
+    var input, filter;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+
+    axios.get("http://127.0.0.1:3000/allTasks")
+        .then(function(response) {
+            // handle success
+            // console.log(response.data);
+            let result = []
+            response.data.map((sData) => {
+                if (sData.title.toUpperCase().indexOf(filter) > -1) {
+                    result.push(sData)
+                }
+            })
+            console.log(result);
+            addT(result);
+        })
+        .catch(function(error) {
+            // handle error
+            console.log(error);
+        })
+        .then(function() {
+            // always executed
+        });
+}
+
+
+
+// Add Elements and data to list
+function addT(details) {
+    $("#demo").empty();
+    details.forEach(todofunc);
+}
+
+
+function todofunc(taskData) {
+    var newElement1 = document.createElement('li');
+    newElement1.className = 'flex-container2';
+
+    var newETitle = document.createElement('span');
+    newETitle.innerHTML = taskData.title;
+    newETitle.className = 'fontbold';
+    newElement1.appendChild(newETitle);
+
+
+
+    var newEc = document.createElement('input');
+    newEc.className = 'cb';
+    newEc.type = 'checkbox';
+
+    newEc.id = taskData.id;
+    if (taskData.status) {
+        newEc.checked = true;
+    } else {
+        newEc.checked = false;
+    }
+    if (taskData.status) {
+        newElement1.id = 'linethrough';
+    } else {
+        newElement1.id = '';
+    }
+
+    newElement1.appendChild(newEc);
+
+    newElement2 = document.createElement('div');
+    newElement2.innerHTML = ' ' + taskData.desc + '<hr>';
+    newElement1.appendChild(newElement2);
+    var lii = document.getElementById('demo');
+    lii.appendChild(newElement1);
+
+    $("#" + taskData.id).change(function() {
+
+        axios.put('http://127.0.0.1:3000/editTask', {
+                id: taskData.id,
+                status: !taskData.status,
+
+            })
+            .then(function(response) {
+                getData()
+
+            })
+            .catch(function(error) {
+                console.log(error);
+            });
+
+    })
+
+
+
+
+}
+
+
+function getData() {
+    axios.get("http://127.0.0.1:3000/allTasks")
+        .then(function(response) {
+            // handle success
+            // console.log(response.data);
+            addT(response.data)
+        })
+        .catch(function(error) {
+            // handle error
+            console.log(error);
+        })
+        .then(function() {
+            // always executed
+        });
+}
+
+
+
+
+
+$('#pend').click(function() {
+    console.log('hii');
+
+
+    axios.get("http://127.0.0.1:3000/allTasks")
+        .then(function(response) {
+            // handle success
+            // console.log(response.data);
+            let result2 = [];
+
+            response.data.map((sData) => {
+                if (sData.status == false) {
+                    result2.push(sData)
+                }
+            })
+            console.log(result2);
+            addT(result2);
+        })
+        .catch(function(error) {
+            // handle error
+            console.log(error);
+        })
+        .then(function() {
+            // always executed
+        });
+
+})
+
+$('#comp').click(function() {
+
+
+    axios.get("http://127.0.0.1:3000/allTasks")
+        .then(function(response) {
+            // handle success
+            // console.log(response.data);
+            let result3 = [];
+
+            response.data.map((sData) => {
+                if (sData.status == true) {
+                    result3.push(sData)
+                }
+            })
+            console.log(result3);
+            addT(result3);
+        })
+        .catch(function(error) {
+            // handle error
+            console.log(error);
+        })
+        .then(function() {
+            // always executed
+        });
+})
